f4cf9bc272b89a101ef9c6f79e330ce74506f440
Aleksandr Kim <k1mal3xandre@yandex.ru>
2024-10-07 10:34:11 +0800

    module_2_5 hotfix_1

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,12 @@
 def get_multiplied_digits(number ):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) > 1:
+    if (number % 10 == 0) and (len(str_number) > 1):
+       return get_multiplied_digits(int(str_number[: -1]))
+    elif len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(40200)
 print(result)
